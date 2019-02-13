9d8e6df725c4bdaa3cec9d528402efcfe83885c9
ManlyMarco <dummyaccx@gmail.com>
2019-02-13 16:41:57 +0100

    Clarify version const field

--- KKAPI/Properties/AssemblyInfo.cs
+++ KKAPI/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using KKAPI;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion(KoikatuAPI.Version)]
+[assembly: AssemblyFileVersion(KoikatuAPI.VersionConst)]
