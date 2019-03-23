638a714d1ed75ee52e1ce3e5c7015fd2dfc64e17
ManlyMarco <dummyaccx@gmail.com>
2019-03-23 20:52:54 +0100

    Added GetOCIChar extension

--- KKAPI/Chara/CharacterExtensions.cs
+++ KKAPI/Chara/CharacterExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using KKAPI.Studio;
+using Studio;
 
 namespace KKAPI.Chara
 {
@@ -13,6 +15,17 @@ namespace KKAPI.Chara
         public static ChaControl GetChaControl(this ChaFile chaFile)
         {
             return CharacterApi.ChaControls.FirstOrDefault(x => x.chaFile == chaFile);
+        }
+
+        /// <summary>
+        /// Get GetOCIChar that is assigned to this character. Only works in CharaStudio, returns null elsewhere.
+        /// </summary>
+        public static OCIChar GetOCIChar(this ChaControl chaControl)
+        {
+            if (!StudioAPI.InsideStudio) return null;
+            var infos = global::Studio.Studio.Instance.dicInfo;
+            var charas = infos.Values.OfType<OCIChar>();
+            return charas.FirstOrDefault(x => x.charInfo == chaControl);
         }
     }
 }
