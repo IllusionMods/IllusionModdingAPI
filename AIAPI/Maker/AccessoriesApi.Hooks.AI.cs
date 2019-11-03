ef063ddb762bd1b649e5566e94a9040cbc0e87c4
ManlyMarco <dummyaccx@gmail.com>
2019-11-03 20:18:56 +0100

    Fixed accessory slot id being read wrong in maker

--- AIAPI/Maker/AccessoriesApi.Hooks.AI.cs
+++ AIAPI/Maker/AccessoriesApi.Hooks.AI.cs
@@ -11,7 +11,7 @@ namespace KKAPI.Maker
             [HarmonyPatch(typeof(CvsA_Slot), nameof(CvsA_Slot.ChangeMenuFunc))]
             public static void ChangeMenuFuncPostfix(CvsA_Slot __instance)
             {
-                OnSelectedMakerSlotChanged(__instance, GetSlotIndex(__instance));
+                OnSelectedMakerSlotChanged(__instance, __instance.SNo);
             }
 
             [HarmonyPostfix]
@@ -25,18 +25,7 @@ namespace KKAPI.Maker
             [HarmonyPatch(typeof(CvsA_Slot), nameof(CvsA_Slot.ChangeAcsType), typeof(int))]
             public static void ChangeAcsTypePostfix(CvsA_Slot __instance)
             {
-                OnAccessoryKindChanged(__instance, GetSlotIndex(__instance));
-            }
-
-            private static int GetSlotIndex(CvsA_Slot __instance)
-            {
-                var tr = Traverse.Create(__instance);
-                // Compatibility with trial, in main game it changed to a property
-                var prop = tr.Field("SNo");
-                if (!prop.FieldExists())
-                    prop = tr.Property("SNo");
-
-                return prop.GetValue<int>();
+                OnAccessoryKindChanged(__instance, __instance.SNo);
             }
         }
     }
