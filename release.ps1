14d393c473ed6b2a6908370f50e572e270f5fe73
ManlyMarco <dummyaccx@gmail.com>
2019-11-08 17:06:14 +0100

    Script tweaks

--- release.ps1
+++ release.ps1
@@ -1,12 +1,18 @@
-$dir = $PSScriptRoot + "\bin\"
+$array = @("KKAPI", "ECAPI", "AIAPI")
+
+if ($PSScriptRoot -match '.+?\\bin\\?') {
+    $dir = $PSScriptRoot + "\"
+}
+else {
+    $dir = $PSScriptRoot + "\bin\"
+}
+
 $out = $dir + "BepInEx\plugins\" 
 
-$ver = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($dir +"KKAPI.dll").FileVersion.ToString()
+$ver = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($dir + $array[0] + ".dll").FileVersion.ToString()
 
 
 New-Item -ItemType Directory -Force -Path ($dir + "out\")
-
-$array = @("KKAPI", "ECAPI", "AIAPI")
 
 foreach ($element in $array) {
 
@@ -14,7 +20,7 @@ Remove-Item -Force -Path ($out + "*") -ErrorAction SilentlyContinue
 New-Item -ItemType Directory -Force -Path $out
 
 Copy-Item -Path ($dir + $element + ".dll") -Destination $out
-Copy-Item -Path ($dir + $element + ".xml") -Destination $out
+Copy-Item -Path ($dir + $element + ".xml") -Destination $out -ErrorAction SilentlyContinue
 
 Compress-Archive -Path ($dir + "BepInEx") -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + $element + "_" + $ver + ".zip")
 
