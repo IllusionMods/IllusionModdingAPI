2ba9ed373d2d9819c4a76dbefe6ef6a145969208
krypto5863 <29824718+krypto5863@users.noreply.github.com>
2024-09-17 15:41:50 -0400

    Added dropdowns and colorpickers to the SceneEffectCategory stuff (#77)

--- src/Shared.CharaStudio/Studio/UI/SceneEffectsCategory.cs
+++ src/Shared.CharaStudio/Studio/UI/SceneEffectsCategory.cs
@@ -7,6 +7,7 @@ using KKAPI.Utilities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
@@ -17,7 +18,7 @@ using Text = TMPro.TextMeshProUGUI;
 namespace KKAPI.Studio.UI
 {
     /// <summary>
-    /// Class that adds a new subcategory to the Scene Effects menu. Create a new instance and then add SliderSets and ToggleSets.
+    /// Class that adds a new subcategory to the Scene Effects menu. Create a new instance and then add SliderSets, ToggleSets, DropdownSets, ColorPickerSets, or just plain LabelSets.
     /// </summary>
     public class SceneEffectsCategory
     {
@@ -30,8 +31,13 @@ namespace KKAPI.Studio.UI
         private static GameObject _toggleSource;
         private static GameObject _sliderSource;
 #if !PH
+        private static GameObject _dropdownSource;
+        private static GameObject _colorPickerSource;
         private static GameObject _inputSource;
         private static GameObject _buttonSource;
+
+        private static Sprite _expandSprite;
+        private static Sprite _collapseSprite;
 #endif
 
         private static void Initialize()
@@ -46,12 +52,16 @@ namespace KKAPI.Studio.UI
             const string sliderSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Slider Focal Size";
             const string inputSourcePath = "Screen Effect/Viewport/Content/Depth of Field/InputField Focal Size";
             const string buttonSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Button Focal Size Default";
+            const string dropdownSourcePath = "Screen Effect/Viewport/Content/Sky/Pattern/Dropdown";
+            const string colorPickerSourcePath = "Screen Effect/Viewport/Content/Amplify Occlusion Effect/Button Color";
 #elif AI || HS2
             const string labelSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Draw/TextMeshPro";
             const string toggleSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Draw/Toggle";
             const string sliderSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Focal Size/Slider";
             const string inputSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Focal Size/InputField";
             const string buttonSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Focal Size/Button Default";
+            const string dropdownSourcePath = "Screen Effect/Viewport/Content/Sky/Pattern/Dropdown";
+            const string colorPickerSourcePath = "Screen Effect/Viewport/Content/Ambient Occlusion/Color/Button";
 #elif PH
             const string labelSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Text Draw";
             const string toggleSourcePath = "Screen Effect/Viewport/Content/Depth of Field/Toggle Draw";
@@ -68,6 +78,13 @@ namespace KKAPI.Studio.UI
 #if !PH
             _inputSource = sef.transform.Find(inputSourcePath)?.gameObject ?? throw new ArgumentException("Could not find " + inputSourcePath);
             _buttonSource = sef.transform.Find(buttonSourcePath)?.gameObject ?? throw new ArgumentException("Could not find " + buttonSourcePath);
+            _dropdownSource = sef.transform.Find(dropdownSourcePath)?.gameObject ?? throw new ArgumentException("Could not find " + dropdownSourcePath);
+            _colorPickerSource = sef.transform.Find(colorPickerSourcePath)?.gameObject ?? throw new ArgumentException("Could not find " + colorPickerSourcePath);
+
+            //Not too proud of these, but they work fine and should continue to work fine.
+            var allSprites = Resources.FindObjectsOfTypeAll<Sprite>();
+            _expandSprite = allSprites.FirstOrDefault(r => r.name.Equals("sp_sn_09_00_05"));
+            _collapseSprite = allSprites.FirstOrDefault(r => r.name.Equals("sp_sn_09_00_03"));
 #endif
 
             _wasInitialized = true;
@@ -83,6 +100,10 @@ namespace KKAPI.Studio.UI
         public GameObject Content { get; set; }
 
         /// <summary>
+        /// Labels that have been added.
+        /// </summary>
+        public List<SceneEffectsLabelSet> Labels = new List<SceneEffectsLabelSet>();
+        /// <summary>
         /// Toggles that have been added.
         /// </summary>
         public List<SceneEffectsToggleSet> Toggles = new List<SceneEffectsToggleSet>();
@@ -90,6 +111,14 @@ namespace KKAPI.Studio.UI
         /// Sliders that have been added.
         /// </summary>
         public List<SceneEffectsSliderSet> Sliders = new List<SceneEffectsSliderSet>();
+        /// <summary>
+        /// Dropdowns that have been added.
+        /// </summary>
+        public List<SceneEffectsDropdownSet> Dropdowns = new List<SceneEffectsDropdownSet>();
+        /// <summary>
+        /// Color pickers that have been added.
+        /// </summary>
+        public List<SceneEffectsColorPickerSet> ColorPickers = new List<SceneEffectsColorPickerSet>();
 
         /// <summary>
         /// Create a new Screen Effects subcategory.
@@ -115,8 +144,43 @@ namespace KKAPI.Studio.UI
             layoutElement.preferredHeight = 0;
             layoutElement.preferredWidth = 375;
 
+#if !PH
+            //No clue if PH even has these buttons so marked it out.
+            var button = Header.GetComponentInChildren<Button>();
+            button.onClick.AddListener(() =>
+            {
+                Content.SetActive(!Content.activeSelf);
+                button.image.sprite = Content.activeSelf ? _collapseSprite: _expandSprite;
+            });
+#endif
+
             foreach (Transform child in Content.transform)
                 Object.Destroy(child.gameObject);
+        }
+
+        /// <summary>
+        /// Add a label to the category, can be used for sectioning.
+        /// </summary>
+        /// <param name="text">Label text</param>
+        /// <returns>Instance of the LabelSet</returns>
+        public SceneEffectsLabelSet AddLabelSet(string text)
+        {
+            var containingElement = new GameObject().AddComponent<RectTransform>();
+            containingElement.name = text;
+            containingElement.SetParent(Content.transform, false);
+
+            KoikatuAPI.Instance.StartCoroutine(SetPosDelayed(containingElement.transform, GetCurrentOffset()));
+
+            var label = Object.Instantiate(_labelSource).GetComponent<Text>();
+            label.transform.SetParent(containingElement.transform, false);
+            label.transform.localPosition = new Vector3(4f, 0f, 0f);
+
+            var labelSet = new SceneEffectsLabelSet(label, text);
+            Labels.Add(labelSet);
+
+            CorrectCategoryScale();
+
+            return labelSet;
         }
 
         /// <summary>
@@ -196,6 +260,70 @@ namespace KKAPI.Studio.UI
             return sliderSet;
         }
 
+#if !PH
+        /// <summary>
+        /// Add a dropdown to this Screen Effects subcategory.
+        /// </summary>
+        /// <param name="text">Label text</param>
+        /// <param name="setter">Method to be called when the dropdown selection changes.</param>
+        /// <param name="options">A list of the options to display in the dropdown.</param>
+        /// <param name="initialValue">The initial value to be selected in the dropdown.</param>
+        /// <returns>Instance of the DropdownSet</returns>
+        public SceneEffectsDropdownSet AddDropdownSet(string text, Action<int> setter, List<string> options, string initialValue)
+        {
+            var containingElement = new GameObject().AddComponent<RectTransform>();
+            containingElement.name = text;
+            containingElement.SetParent(Content.transform, false);
+
+            KoikatuAPI.Instance.StartCoroutine(SetPosDelayed(containingElement.transform, GetCurrentOffset()));
+
+            var label = Object.Instantiate(_labelSource).GetComponent<Text>();
+            label.transform.SetParent(containingElement.transform, false);
+            label.transform.localPosition = new Vector3(4f, 0f, 0f);
+
+            var dropDown = Object.Instantiate(_dropdownSource).GetComponent<Dropdown>();
+            dropDown.transform.SetParent(containingElement.transform, false);
+            dropDown.transform.localPosition = new Vector3(160f, 0f, 0f);
+
+            var dropDownSet = new SceneEffectsDropdownSet(label, dropDown, text, setter, options, initialValue);
+            Dropdowns.Add(dropDownSet);
+
+            CorrectCategoryScale();
+
+            return dropDownSet;
+        }
+        /// <summary>
+        /// Add a color picker to this category.
+        /// </summary>
+        /// <param name="text">The UI label text.</param>
+        /// <param name="setter">Function to be called when the color changes.</param>
+        /// <param name="initialValue">The initial color to display.</param>
+        /// <returns>Instance of the ColorPickerSet</returns>
+        public SceneEffectsColorPickerSet AddColorPickerSet(string text, Action<Color> setter, Color initialValue)
+        {
+            var containingElement = new GameObject().AddComponent<RectTransform>();
+            containingElement.name = text;
+            containingElement.SetParent(Content.transform, false);
+
+            KoikatuAPI.Instance.StartCoroutine(SetPosDelayed(containingElement.transform, GetCurrentOffset()));
+
+            var label = Object.Instantiate(_labelSource).GetComponent<Text>();
+            label.transform.SetParent(containingElement.transform, false);
+            label.transform.localPosition = new Vector3(4f, 0f, 0f);
+
+            var colorPicker = Object.Instantiate(_colorPickerSource).GetComponent<Button>();
+            colorPicker.transform.SetParent(containingElement.transform, false);
+            colorPicker.transform.localPosition = new Vector3(160f, 0f, 0f);
+
+            var colorPickerSet = new SceneEffectsColorPickerSet(label, colorPicker, text, setter, initialValue);
+            ColorPickers.Add(colorPickerSet);
+
+            CorrectCategoryScale();
+
+            return colorPickerSet;
+        }
+#endif
+
         private void CorrectCategoryScale()
         {
             var layoutElement = Content.GetComponent<LayoutElement>();
@@ -214,6 +342,6 @@ namespace KKAPI.Studio.UI
             tr.localPosition = v;
         }
 
-        private float GetCurrentOffset() => OffsetMultiplier * (Toggles.Count + Sliders.Count);
+        private float GetCurrentOffset() => OffsetMultiplier * (Toggles.Count + Sliders.Count + Dropdowns.Count + ColorPickers.Count + Labels.Count);
     }
 }
---
+++ src/Shared.CharaStudio/Studio/UI/SceneEffectsCategoryDropDown.cs
@@ -0,0 +1,141 @@
+﻿#if KK || KKS || AI || HS2
+#define TMP
+#endif
+
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+#if TMP
+using Text = TMPro.TextMeshProUGUI;
+#endif
+
+namespace KKAPI.Studio.UI
+{
+    /// <summary>
+    /// A container for the value of a dropdown, associated label and dropdown, and the setter method that triggers on value change.
+    /// </summary>
+    public class SceneEffectsDropdownSet
+    {
+        private int CurrentValue = -1;
+        private bool EventsEnabled = true;
+
+        #region Backing Fields
+
+        private string _text;
+
+        #endregion Backing Fields
+
+        /// <summary>
+        /// Label UI element.
+        /// </summary>
+        public Text Label { get; set; }
+        /// <summary>
+        /// DropDown UI element.
+        /// </summary>
+        public Dropdown Dropdown { get; set; }
+        /// <summary>
+        /// Method called when the value of the Dropdown is changed.
+        /// </summary>
+        public Action<int> Setter { get; set; }
+        /// <summary>
+        /// Get or set the text of the label.
+        /// </summary>
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                Label.text = value;
+            }
+        }
+        /// <summary>
+        /// Get or set the value of the Dropdown.
+        /// </summary>
+        public int Value
+        {
+            get => GetValue();
+            set => SetValue(value);
+        }
+
+        /// <summary>
+        /// Create a new Dropdown. Typically, you want to use SceneEffectsCategory.AddDropdown instead of creating these manually.
+        /// </summary>
+        /// <param name="label">Label UI element</param>
+        /// <param name="dropdown">Dropdown component</param>
+        /// <param name="text">Label text</param>
+        /// <param name="setter">Method that will be called on value change</param>
+        /// <param name="optionsList">List of options the Dropdown will display.</param>
+        /// <param name="initialValue">The initial value to show on the Dropdown.</param>
+        public SceneEffectsDropdownSet(Text label, Dropdown dropdown, string text, Action<int> setter, List<string> optionsList, string initialValue = "")
+        {
+            Label = label;
+            Dropdown = dropdown;
+            Setter = setter;
+            Dropdown.ClearOptions();
+            Dropdown.AddOptions(optionsList);
+            Text = text;
+
+            if (initialValue.IsNullOrWhiteSpace() == false)
+            {
+                Dropdown.value = optionsList.FindIndex(r => r.Equals(initialValue));
+                Dropdown.RefreshShownValue();
+            }
+
+            Dropdown.onValueChanged.RemoveAllListeners();
+            Dropdown.onValueChanged.AddListener(delegate (int value)
+            {
+                if (!EventsEnabled) return;
+                Value = value;
+            });
+
+            Label.gameObject.name = $"Label {Text}";
+            Dropdown.gameObject.name = $"Dropdown {Text}";
+        }
+
+        /// <summary>
+        /// Get the value of the Dropdown.
+        /// </summary>
+        /// <returns>Value of the Dropdown</returns>
+        public int GetValue() => CurrentValue;
+        /// <summary>
+        /// Set the value of the Dropdown and trigger the Setter method.
+        /// </summary>
+        /// <param name="value">Value to set the Dropdown</param>
+        public void SetValue(int value) => SetValue(value, true);
+        /// <summary>
+        /// Set the value of the Dropdown.
+        /// </summary>
+        /// <param name="value">Value to set the Dropdown</param>
+        /// <param name="triggerEvents">Whether to trigger the Setter method</param>
+        public void SetValue(int value, bool triggerEvents)
+        {
+            EventsEnabled = false;
+            CurrentValue = value;
+            Dropdown.value = value;
+            Dropdown.RefreshShownValue();
+            EventsEnabled = true;
+            if (triggerEvents)
+                Setter.Invoke(value);
+        }
+        /*
+        /// <summary>
+        /// Reset the toggle to the initial value and trigger the Setter method.
+        /// </summary>
+        public void Reset() => Reset(true);
+        /// <summary>
+        /// Reset the toggle to the initial value.
+        /// </summary>
+        /// <param name="triggerEvents">Whether to trigger the Setter method</param>
+        public void Reset(bool triggerEvents)
+        {
+            EventsEnabled = false;
+            CurrentValue = InitialValue;
+            Toggle.isOn = InitialValue;
+            EventsEnabled = true;
+            if (triggerEvents)
+                Setter.Invoke(InitialValue);
+        }
+        */
+    }
+}
