ee7362c2e4c72d574e84ccddc6b2e2c348cd6e34
starstormhun <markus.tamas98@gmail.com>
2025-10-26 04:51:10 +0100

    LocalTextures Maker support for PH / EC / KK / KKS
    AI / HS2 todo

---
+++ src/PHAPI/Maker/Events/LocalSaveChangedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+#pragma warning disable 1591
+
+namespace KKAPI.Maker
+{
+    /// <summary>
+    /// Event argument used for when save type for cards is toggled
+    /// </summary>
+    public sealed class LocalSaveChangedEventArgs : EventArgs
+    {
+        public LocalSaveChangedEventArgs(LocalTextures.TextureSaveType saveType)
+        {
+            NewSetting = saveType;
+        }
+
+        /// <summary>
+        /// The new state of the setting
+        /// </summary>
+        public LocalTextures.TextureSaveType NewSetting { get; }
+    }
+}
---
+++ src/PHAPI/Maker/LocalTextures.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx.Configuration;
+using KKAPI.Utilities;
+
+namespace KKAPI.Maker
+{
+    /// <summary>
+    /// API for universal toggling of locally saved textures.
+    /// </summary>
+    public static class LocalTextures
+    {
+        /// <summary>
+        /// Fired whenever the SaveType changes.
+        /// </summary>
+        public static System.EventHandler SaveTypeChangedEvent;
+
+        /// <summary>
+        /// The type of texture saving that plugins should use
+        /// </summary>
+        public static TextureSaveType SaveType
+        {
+            get
+            {
+                return ConfTexSaveType.Value;
+            }
+            set
+            {
+                ConfTexSaveType.Value = value;
+                var eLogger = ApiEventExecutionLogger.GetEventLogger();
+                eLogger.Begin(nameof(SaveTypeChangedEvent), "");
+                OnSaveTypeChanged(null, new LocalSaveChangedEventArgs(value), eLogger);
+                eLogger.End();
+            }
+        }
+
+        /// <summary>
+        /// Options for the type of texture saving that plugins should use
+        /// </summary>
+        public enum TextureSaveType
+        {
+            /// <summary>
+            /// Textures should be bundled with the card
+            /// </summary>
+            Bundled = 0,
+            /// <summary>
+            /// Textures should be saved separately from the card in a local folder
+            /// </summary>
+            Local = 1,
+        }
+
+        internal static ConfigEntry<TextureSaveType> ConfTexSaveType { get; private set; }
+
+        static LocalTextures()
+        {
+            ConfTexSaveType = KoikatuAPI.Instance.Config.Bind("Local Textures", "Card Save Type", TextureSaveType.Bundled, new ConfigDescription("Whether external textures used by plugins should be bundled with the card or to a local folder."));
+        }
+
+        private static void OnSaveTypeChanged(object sender, System.EventArgs args, ApiEventExecutionLogger eventLogger)
+        {
+            SaveTypeChangedEvent.SafeInvokeWithLogging(handler => handler.Invoke(sender, args), nameof(SaveTypeChangedEvent), eventLogger);
+        }
+    }
+
+
+}
