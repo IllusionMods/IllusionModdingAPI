ec24af3fded68f38743bec381efd97780ce82c52
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2026-02-14 21:39:15 +0100

    Bump version to 1.46

--- src/Shared.Core/Constants.cs
+++ src/Shared.Core/Constants.cs
@@ -2,7 +2,7 @@ namespace KKAPI
 {
     internal static class Constants
     {
-        public const string Version = "1.45.1";
+        public const string Version = "1.46";
 
         public const string GUID = "marco.kkapi";
     }
